75acd5bc475d70499f9e7bb5ce7f683f2e889267
Arthur Gibert <arthurgibert@free.fr>
2024-02-02 10:30:22 +0100

    Ajout de la méthode displayMenu

--- src/fr/fms/jdbc/Shop.java
+++ src/fr/fms/jdbc/Shop.java
@@ -1,19 +1,22 @@
 package fr.fms.jdbc;
 
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import fr.fms.dao.ArticleDao;
 import fr.fms.dao.UserDao;
+import fr.fms.entities.Account;
 import fr.fms.entities.Article;
 import fr.fms.entities.User;
 
 public class Shop {
 	public static void main(String[] args) {
 		
-		System.out.println("Bonjour et bienvenu chez DIGITAL DEPOT");
+		System.out.println("-------- DIGITAL DEPOT --------");
 		User userCurrent = userConnect();
-		System.out.println(userCurrent);
+		System.out.println("Bonjour et bienvenu " + userCurrent.getLogin() + " !");
+		displayMenu(userCurrent);
 	}
 	
 	/**
@@ -45,6 +48,56 @@ public class Shop {
 			
 			currentUser = daoUser.userExists(login, password);
 		}
-		return currentUser;
+		scanner.close();
+		return currentUser;	
+	}
+	
+	/**
+	 * Méthode permettant d'afficher le menu principal
+	 * @param userCurrent est l'utilisateur connecter
+	 */
+	public static void displayMenu(User userCurrent) {
+		ArticleDao daoArticle = new ArticleDao();
+		int userChoice;
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("------------------- taper le numéro correspondant -----------------------");
+		System.out.println("1: Ajouter un article - 2: Retirer un article - 3: Afficher notre panier - 4: Payer notre commande - 5: Sortir");
+		
+		while(true) {
+			try {
+				userChoice = scanner.nextInt();
+				if(userChoice > 0 && userChoice < 6) {
+					break;
+				} else {
+					System.out.println("Erreur : veuillez saisir numéro de choix valable (1 - 5)");
+				}
+			} catch(InputMismatchException e) {
+				System.out.println("Erreur : veuillez saisir numéro de choix valable (1 - 5)");
+				System.err.print("ERREUR : Saisie de mauvais caractère ");
+				e.printStackTrace();
+				scanner.next();
+			}
+		}
+		
+		switch(userChoice) {
+		case 1:
+			System.out.println("Choix 1");
+			daoArticle.readAll();
+			break;
+		case 2:
+			System.out.println("Choix 2");
+			break;
+		case 3:
+			System.out.println("Choix 3");
+			break;
+		case 4:
+			System.out.println("Choix 4");
+			break;
+		case 5:
+			System.out.println("Choix 5");
+			break;
+		
+		}
 	}
 }
